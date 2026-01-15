7f3b278835e8b820306d8ee8b694f5506de4b0c5
akmaalsyh <taufansyah50@gmail.com>
2026-01-15 14:31:43 +0700

    Membuat object DestinasiHome untuk mendefinisikan rute navigasi dan judul halaman utama (dashboard)

---
+++ app/src/main/java/com/example/simkader_236/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.simkader_236.uicontroller.route
+import com.example.simkader_236.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.dashboard_title
+}
